422019000a62acedd79448cc1de07817aa2676c3
Tobiáš <tobste58@gmail.com>
2023-03-20 14:53:56 +0100

    Almost done ... searching for mistakes begin

--- ipkcpc.cpp
+++ ipkcpc.cpp
@@ -17,8 +17,29 @@
 
 using namespace std;
 
+int client_socket; //can be also not global but its easier to use it in signal handler
+
+void handle_sigint_TCP(int sig) {
+    send(client_socket, "BYE\n", strlen("BYE\n"), 0);
+    char buf[BUFSIZE];
+    int bytesrx = recv(client_socket, buf, BUFSIZE, 0);
+    printf("Echo from server: %s", buf);
+    // Close the socket
+    close(client_socket);
+    exit(0);
+}
+
 void tcpitis(int client_socket, char *buf, struct sockaddr_in server_address, string endit, int bytestx, int bytesrx)
 {
+    int controlPressed = 0;
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = handle_sigint_TCP;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    socklen_t server_address_length = sizeof(server_address);
+
     if (connect(client_socket, (const struct sockaddr *) &server_address, sizeof(server_address)) != 0)
         {
             perror("ERROR: connect");
@@ -29,7 +50,12 @@ void tcpitis(int client_socket, char *buf, struct sockaddr_in server_address, st
     {   
         bzero(buf, BUFSIZE);
         printf("Please enter msg: ");
+        if (controlPressed == 0 && feof(stdin)) {
+            buf= "BYE\n";
+        }
+        else {
         fgets(buf, BUFSIZE, stdin); 
+        }    
         int buf_len = strlen(buf);
         buf[buf_len]=(char)10;
 
@@ -52,16 +78,33 @@ void tcpitis(int client_socket, char *buf, struct sockaddr_in server_address, st
     }
 }
 
+void handle_sigint_UDP(int sig) {
+    printf("\n");
+    close(sig);
+    exit(0);
+}
 
-void udpitis(int client_socket, char *buf, struct sockaddr_in server_address, string endit, int bytestx, int bytesrx)
-{
+void udpitis(int client_socket, char *buf, struct sockaddr_in server_address, string endit, int bytestx, int bytesrx) {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = handle_sigint_UDP;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
     socklen_t server_address_length = sizeof(server_address);
+    bool done = false;
 
-    while (1)
-    {
+    while (!done) {
         bzero(buf, BUFSIZE);
         printf("Please enter msg: ");
-        fgets(buf + 2, BUFSIZE - 2, stdin); 
+        fgets(buf + 2, BUFSIZE - 2, stdin);
+
+        if (feof(stdin)) {
+            printf("\n");
+            close(client_socket);
+            done = true;
+            continue;
+        }
 
         int msg_len = strlen(buf + 2);
         buf[0] = '\0';
@@ -69,24 +112,22 @@ void udpitis(int client_socket, char *buf, struct sockaddr_in server_address, st
 
         /* odeslani zpravy na server */
         bytestx = sendto(client_socket, buf, msg_len + 2, 0, (struct sockaddr *) &server_address, server_address_length);
-        if (bytestx < 0) 
+        if (bytestx < 0) {
             perror("ERROR in sendto");
+            continue;
+        }
         bzero(buf, BUFSIZE);
 
         /* prijeti odpovedi a jeji vypsani */
         bytesrx = recvfrom(client_socket, buf, BUFSIZE, 0, (struct sockaddr *) &server_address, &server_address_length);
-        if (bytesrx < 0) 
+        if (bytesrx < 0) {
             perror("ERROR in recvfrom");
+            continue;
+        }
 
         printf("Echo from server: %s", buf + 2);
-        if (endit==buf)
-        {
-            close(client_socket);
-        }
     }
 }
-
-
 
 int main(int argc, char *argv[])
 {
@@ -95,7 +136,7 @@ int arg=0;
 string host;
 string port;
 string mode;
-int client_socket, port_number, bytestx, bytesrx;
+int port_number, bytestx, bytesrx;
 const char *server_hostname;
 struct hostent *server;
 struct sockaddr_in server_address;
