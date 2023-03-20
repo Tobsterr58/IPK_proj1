4d3de132efbd2d1f50f938f3c5831e30705a3f66
Tobiáš <tobste58@gmail.com>
2023-03-21 00:07:16 +0100

    Add comments, deleted all debuging outputs

--- ipkcpc.cpp
+++ ipkcpc.cpp
@@ -1,3 +1,9 @@
+/*
+    IPK project 1 (IPK Calculator Protocol Client)
+    Tobiáš Štec (xstect00)
+    2BIT - 2023
+*/
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,16 +25,17 @@ using namespace std;
 
 int client_socket; //can be also not global but its easier to use it in signal handler
 
+//signal handler for TCP (CTRL+C)
 void handle_sigint_TCP(int sig) {
     send(client_socket, "BYE\n", strlen("BYE\n"), 0);
     char buf[BUFSIZE];
     int bytesrx = recv(client_socket, buf, BUFSIZE, 0);
-    printf("Echo from server: %s", buf);
-    // Close the socket
+    printf("%s", buf);
     close(client_socket);
     exit(0);
 }
 
+//tcp function with signal handler for CTRL+D and CTRL+C
 void tcpitis(int client_socket, char *buf, struct sockaddr_in server_address, string endit, int bytestx, int bytesrx)
 {
     int controlPressed = 0;
@@ -48,29 +55,26 @@ void tcpitis(int client_socket, char *buf, struct sockaddr_in server_address, st
 
     while (endit!=buf)
     {   
-        bzero(buf, BUFSIZE);
-        printf("Please enter msg: ");
+        bzero(buf, BUFSIZE); //clears buffer
         if (controlPressed == 0 && feof(stdin)) {
             buf= "BYE\n";
         }
         else {
-        fgets(buf, BUFSIZE, stdin); 
+        fgets(buf, BUFSIZE, stdin); //gets input from stdin
         }    
         int buf_len = strlen(buf);
-        buf[buf_len]=(char)10;
+        buf[buf_len]=(char)10; //adds new line to the end of input
 
-        /* odeslani zpravy na server */
-        bytestx = send(client_socket, buf, strlen(buf), 0);
+        bytestx = send(client_socket, buf, strlen(buf), 0); //sends input to server
         if (bytestx < 0) 
         perror("ERROR in sendto");
         bzero(buf, BUFSIZE);
         
-        /* prijeti odpovedi a jeji vypsani */
-        bytesrx = recv(client_socket, buf, BUFSIZE, 0);
+        bytesrx = recv(client_socket, buf, BUFSIZE, 0); //gets response from server
         if (bytesrx < 0) 
         perror("ERROR in recvfrom");
         
-        printf("Echo from server: %s", buf);
+        printf("%s", buf);
         if (endit==buf)
         {
             close(client_socket);
@@ -78,12 +82,14 @@ void tcpitis(int client_socket, char *buf, struct sockaddr_in server_address, st
     }
 }
 
+//signal handler for UDP (CTRL+C)
 void handle_sigint_UDP(int sig) {
     printf("\n");
     close(sig);
     exit(0);
 }
 
+//udp function with signal handler for CTRL+D and CTRL+C
 void udpitis(int client_socket, char *buf, struct sockaddr_in server_address, string endit, int bytestx, int bytesrx) {
     struct sigaction sigIntHandler;
     sigIntHandler.sa_handler = handle_sigint_UDP;
@@ -96,36 +102,33 @@ void udpitis(int client_socket, char *buf, struct sockaddr_in server_address, st
 
     while (!done) {
         bzero(buf, BUFSIZE);
-        printf("Please enter msg: ");
         fgets(buf + 2, BUFSIZE - 2, stdin);
 
-        if (feof(stdin)) {
+        if (feof(stdin)) { // CTRL+D
             printf("\n");
             close(client_socket);
             done = true;
             continue;
         }
 
-        int msg_len = strlen(buf + 2);
-        buf[0] = '\0';
-        buf[1] = (char) msg_len;
+        int msg_len = strlen(buf + 2); // +2 because of the first two bytes
+        buf[0] = '\0'; // first byte is always 0
+        buf[1] = (char) msg_len; // second byte is the length of the message
 
-        /* odeslani zpravy na server */
-        bytestx = sendto(client_socket, buf, msg_len + 2, 0, (struct sockaddr *) &server_address, server_address_length);
+        bytestx = sendto(client_socket, buf, msg_len + 2, 0, (struct sockaddr *) &server_address, server_address_length); //sends input to server
         if (bytestx < 0) {
             perror("ERROR in sendto");
             continue;
         }
         bzero(buf, BUFSIZE);
 
-        /* prijeti odpovedi a jeji vypsani */
-        bytesrx = recvfrom(client_socket, buf, BUFSIZE, 0, (struct sockaddr *) &server_address, &server_address_length);
+        bytesrx = recvfrom(client_socket, buf, BUFSIZE, 0, (struct sockaddr *) &server_address, &server_address_length); //gets response from server
         if (bytesrx < 0) {
             perror("ERROR in recvfrom");
             continue;
         }
 
-        printf("Echo from server: %s", buf + 2);
+        printf("%s", buf + 2);
     }
 }
 
@@ -145,14 +148,21 @@ string endit = "BYE\n";
 
 char buf[BUFSIZE];
 
-const struct option longopts[] =
+//getopt_long for parsing arguments
+const struct option longopts[] = 
   {
     {"host",      required_argument,  0, 'h'},
     {"port",      required_argument,  0, 'p'},
     {"mode",      required_argument,  0, 'm'},
     {0,0,0,0}
   };
-    while ((arg = getopt_long(argc, argv, "h:p:m:", longopts, NULL))!=-1)
+    if (argc != 7) //checks if there are 7 arguments
+    {
+        fprintf(stderr, "Usage: [-h host] [-p port] [-m mode]\n");
+        return EXIT_FAILURE;
+    }
+    
+    while ((arg = getopt_long(argc, argv, "h:p:m:", longopts, NULL))!=-1) //parses arguments
     {
         switch (arg)
         {
@@ -173,26 +183,27 @@ const struct option longopts[] =
     }
 
 
-    if (host.empty() || port.empty() || mode.empty())
+    if (host.empty() || port.empty() || mode.empty()) //checks if arguments are empty
     {
         fprintf(stderr, "Usage: [-h host] [-p port] [-m mode]\n");
         return EXIT_FAILURE;
     }
 
-    port_number = atoi(port.c_str());
-    server_hostname = host.c_str();
+    port_number = atoi(port.c_str()); //converts port to int
+    server_hostname = host.c_str(); //converts host to const char*
 
-    if ((server = gethostbyname(server_hostname)) == NULL) {
+    if ((server = gethostbyname(server_hostname)) == NULL) { //checks if host is valid
         fprintf(stderr,"ERROR: no such host as %s\n", server_hostname);
         exit(EXIT_FAILURE);
     }
 
 
-    bzero((char *) &server_address, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
-    server_address.sin_port = htons(port_number);
+    bzero((char *) &server_address, sizeof(server_address)); //clears server_address
+    server_address.sin_family = AF_INET; //sets server_address to AF_INET (IPv4) ... AF_INET6 for IPv6 but we don't need that because of the assignment
+    bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length); //copies server address to server_address
+    server_address.sin_port = htons(port_number); //sets port number to server_address
 
+    //checks if mode is tcp or udp
     if (mode == "tcp")
     {
         if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0)
